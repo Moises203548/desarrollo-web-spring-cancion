dc6403f54c606f6e6accb9494780378c2f6f212c
Moises203548 <mortegas4@unicartagena.edu.co>
2026-09-16 22:28:46 -0500

    chore: inicializar proyecto Spring Boot con Web, JPA, Thymeleaf, Security y PostgreSQL

---
+++ cancionapp/src/main/java/edu/unicartagena/cancionapp/CancionappApplication.java
@@ -0,0 +1,13 @@
+package edu.unicartagena.cancionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancionappApplication.class, args);
+	}
+
+}
---
+++ cancionapp/src/test/java/edu/unicartagena/cancionapp/CancionappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unicartagena.cancionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CancionappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
